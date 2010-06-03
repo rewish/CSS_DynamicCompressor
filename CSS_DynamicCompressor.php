7c5ca5acb1a6bd1aac43e841e7cd8b564ff8315a
rewish <rewish.org@gmail.com>
2010-06-03 20:44:18 +0900

    CSS3のプロパティにprefixを付ける機能を追加。デフォルト無効。

--- CSS_DynamicCompressor.php
+++ CSS_DynamicCompressor.php
@@ -4,7 +4,7 @@
  *
  * PHP versions 5
  *
- * @version      0.1
+ * @version      0.2
  * @author       rew <rewish.org@gmail.com>
  * @copyright    (c) 2010 rewish
  * @link         http://rewish.org/php_mysql/css_dynamic_compressor
@@ -12,15 +12,45 @@
  */
 class CSS_DynamicCompressor
 {
-	private $_directory, $_cssFiles, $_css, $_lastModified;
-	private $_comment = array();
-	private $_baseUrl = null;
-	private $_charset = 'UTF-8';
-	private $_target  = 'import.css';
-	private $_cache   = '_cache.css';
-	private $_expireDay = 30;
+	const VERSION = '0.2';
 
-	private function __construct() {}
+	protected
+		$_directory,
+		$_cssFiles,
+		$_css,
+		$_lastModified,
+		$_comment = array(),
+		$_baseUrl = null,
+		$_charset = 'UTF-8',
+		$_target = 'import.css',
+		$_cache = '_cache.css',
+		$_expireDay = 30,
+		$_css3Fix = false,
+		$_css3Fixes = array(
+			// border-radius
+			'/border-([a-z]+)?(-)?([a-z]+)?(-)?radius:([^;\}]+)/' => array(
+				'border-$1$2$3$4radius:$5',
+				'-webkit-border-$1$2$3$4radius:$5',
+				'-moz-border-radius$2$1$3:$5'
+			),
+			// other properties
+			'/(transform|box-|border-image|column-)([\-a-z]+)?:([^;\}]+)/' => array(
+				'$1$2:$3',
+				'-webkit-$1$2:$3',
+				'-moz-$1$2:$3'
+			),
+			// gradient
+			'/background-image:gradient([^;\}]+)/' => array(
+				'background-image:gradient$1',
+				'background-image:-webkit-gradient$1',
+			),
+			'/background:(.+?)?gradient([^;\}]+)/' => array(
+				'background:$1gradient$2',
+				'background:$1-webkit-gradient$2'
+			)
+		);
+
+	protected function __construct() {}
 
 	public static function getInstance()
 	{
@@ -80,6 +110,12 @@ class CSS_DynamicCompressor
 		return $this;
 	}
 
+	public function setCSS3Fix($use)
+	{
+		$this->_css3Fix = $use;
+		return $this;
+	}
+
 	public function display()
 	{
 		$this->compression();
@@ -95,12 +131,13 @@ class CSS_DynamicCompressor
 		}
 		$this->_concatenation();
 		$this->_compression();
+		$this->_fixCSS3();
 		$this->_addComment();
 		$this->_addCharset();
 		$this->_writeFile($this->_cache, $this->_css);
 	}
 
-	private function _concatenation()
+	protected function _concatenation()
 	{
 		if (empty($this->_cssFiles)) {
 			$this->_extractCSSFiles();
@@ -111,7 +148,7 @@ class CSS_DynamicCompressor
 		}
 	}
 
-	private function _extractCSSFiles()
+	protected function _extractCSSFiles()
 	{
 		$target = $this->_readFile($this->_target);
 		preg_match_all('/@import\s+(url\()?["\']?([^"\';]+)/',
@@ -119,7 +156,7 @@ class CSS_DynamicCompressor
 		$this->_cssFiles = $files[2];
 	}
 
-	private function _compression()
+	protected function _compression()
 	{
 		$css = preg_replace('_(/\*.*?\*/|\s{2,}|[\t\r\n]+)_s', '', $this->_css);
 		$css = str_replace(array(': ', ' :', ' {', ';}', ', '),
@@ -129,7 +166,15 @@ class CSS_DynamicCompressor
 		$this->_css = $css;
 	}
 
-	private function _readFile($file)
+	protected function _fixCSS3()
+	{
+		if (!$this->_css3Fix) return;
+		foreach ($this->_css3Fixes as $exp => $fixes) {
+			$this->_css = preg_replace($exp, implode(';', $fixes), $this->_css);
+		}
+	}
+
+	protected function _readFile($file)
 	{
 		if (!$this->_directory) {
 			$this->setDirectory();
@@ -147,15 +192,15 @@ class CSS_DynamicCompressor
 		return file_get_contents($path);
 	}
 
-	private function _writeFile($file, $data)
+	protected function _writeFile($file, $data)
 	{
 		return file_put_contents($this->_directory . $file, $data, LOCK_EX);
 	}
 
-	private function _isModified()
+	protected function _isModified()
 	{
 		$cachePath = realpath($this->_directory . $this->_cache);
-		if (!$cachePath || !file_exists($cachePath)) {
+		if (!$cachePath || !file_exists($cachePath) || !empty($_GET['no_cache'])) {
 			return true;
 		}
 		$modified = false;
@@ -175,10 +220,11 @@ class CSS_DynamicCompressor
 				$this->_lastModified = $mtime;
 			}
 		}
+		array_pop($this->_cssFiles);
 		return $modified;
 	}
 
-	private function _addHeader()
+	protected function _addHeader()
 	{
 		header('Content-Type: text/css');
 		header('Last-Modified: '. gmdate('D, d M Y H:i:s', $this->_lastModified) .' GMT');
@@ -186,12 +232,12 @@ class CSS_DynamicCompressor
 		ob_start('ob_gzhandler');
 	}
 
-	private function _addCharset()
+	protected function _addCharset()
 	{
 		$this->_css = "@charset \"$this->_charset\";\n$this->_css";
 	}
 
-	private function _addComment()
+	protected function _addComment()
 	{
 		$this->_comment[] = '/**';
 		if ($this->_baseUrl) {
@@ -203,7 +249,7 @@ class CSS_DynamicCompressor
 		$this->_css = implode("\n", $this->_comment) . $this->_css;
 	}
 
-	private function _addFileList()
+	protected function _addFileList()
 	{
 		$this->_comment[] = ' * [File list]';
 		foreach ($this->_cssFiles as $file) {
@@ -216,11 +262,11 @@ class CSS_DynamicCompressor
 		$this->_comment[] = ' *';
 	}
 
-	private function _addCopyright()
+	protected function _addCopyright()
 	{
 		// Jesus...would you really delete this?
 		// Oh my god... X(
-		$this->_comment[] = ' * Powered by ' . __CLASS__;
+		$this->_comment[] = ' * Powered by ' . __CLASS__ . ' - v' . self::VERSION;
 		$this->_comment[] = ' * http://rewish.org/php_mysql/css_dynamic_compressor';
 		$this->_comment[] = ' * (c) 2010 rew <rewish.org@gmail.com>';
 	}
